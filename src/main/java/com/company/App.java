ac92113de16bfc335f2c24f392040a9dab38fd01
James Corcoran <jpcsoccer1234@gmail.com>
2022-03-09 15:28:11 -0500

    commit

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
+//this is a comment
     public static void main(String[] args) { SpringApplication.run(App.class, args); }
 }
