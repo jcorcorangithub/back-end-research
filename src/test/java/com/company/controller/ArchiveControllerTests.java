25e2fa028c5fdaa6a9e256f9f56017d07bf98b9f
Your Name <nickmihaly@gmail.com>
2022-03-10 12:48:14 -0800

    article controller test update

--- src/test/java/com/company/controller/ArchiveControllerTests.java
+++ src/test/java/com/company/controller/ArchiveControllerTests.java
@@ -1,5 +1,6 @@
 package com.company.controller;
 
+import com.company.exceptions.NotFoundException;
 import com.company.model.Archive;
 import com.company.model.Article;
 import com.company.repository.ArchiveRepository;
@@ -17,13 +18,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -80,8 +77,8 @@ public class ArchiveControllerTests{
         when(serviceLayer.saveArchive(inputArchive)).thenReturn(outputArchive);
 
         mockMvc.perform(post("/archive")
-                .content(inputString)
-                .contentType(MediaType.APPLICATION_JSON)
+                        .content(inputString)
+                        .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -89,11 +86,25 @@ public class ArchiveControllerTests{
     }
 
     @Test
+    public void shouldThrow422OnEmptyArchive() throws Exception {
+        Archive emptyArchive = new Archive();
+        String inputEmptyArchive = mapper.writeValueAsString(emptyArchive);
+        when(serviceLayer.saveArchive(emptyArchive)).thenThrow(IllegalArgumentException.class);
+
+        mockMvc.perform(post("/archive")
+                        .content(inputEmptyArchive)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
     public void findAllArchivesTest() throws Exception{
         String outputString = mapper.writeValueAsString(archiveList);
         when(serviceLayer.findAllArchives()).thenReturn(archiveList);
         mockMvc.perform(get("/archive")
-                .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
@@ -105,16 +116,46 @@ public class ArchiveControllerTests{
         String inputString = mapper.writeValueAsString(inputArchive);
         String outputString = mapper.writeValueAsString(outputArchive);
 
-        when(serviceLayer.findArchive(inputArchive.getArchiveId())).thenReturn(outputArchive);
+        when(serviceLayer.findArchive(inputArchive.getArchiveId())).thenReturn(Optional.ofNullable(outputArchive));
 
         mockMvc.perform(get("/archive/1")
-                .content(inputString)
-                .contentType(MediaType.APPLICATION_JSON)
+                        .content(inputString)
+                        .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json(outputString));
     }
+
+    @Test
+    public void shouldReturn422FindById() throws Exception {
+
+        when(serviceLayer.findArchive(8577)).thenThrow(IllegalArgumentException.class);
+
+        mockMvc.perform(get("/archive/8577")
+//                        .content(inputFindArchive)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+//    @Test
+//    public void shouldReturn404FindById() throws Exception {
+//        Archive archive = new Archive();
+//        archive.setArchiveName("archive name");
+//        archive.setArchiveId(111);
+//        String inputFindArchive = mapper.writeValueAsString(archive);
+//
+//        when(serviceLayer.findArchive(archive.getArchiveId())).thenThrow(NotFoundException.class);
+//
+//        mockMvc.perform(get("/archive/-4")
+//                        .content(inputFindArchive)
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                )
+//                .andDo(print())
+//                .andExpect(status().isNotFound());
+//    }
 
     @Test
     public void updateArchiveByIdTest() throws Exception{
@@ -123,7 +164,7 @@ public class ArchiveControllerTests{
         outputArchive.setArchiveId(2);
         String outputString = mapper.writeValueAsString(outputArchive);
 
-        when(serviceLayer.findArchive(inputArchive.getArchiveId())).thenReturn(outputArchive);
+        when(serviceLayer.findArchive(inputArchive.getArchiveId())).thenReturn(Optional.ofNullable(outputArchive));
         when(serviceLayer.updateArchive(inputArchive)).thenReturn(outputArchive);
 
         mockMvc.perform(put("/archive/1")
@@ -134,6 +175,28 @@ public class ArchiveControllerTests{
                 .andExpect(status().isOk())
                 .andExpect(content().json(outputString));
     }
+
+    /////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+    @Test
+    public void shouldReturn422UpdateArchive() throws Exception {
+        Archive newArchive = new Archive();
+        newArchive.setArchiveId(3);
+//        newArchive.setArchiveName("name");
+        String inputString = mapper.writeValueAsString(newArchive);
+
+//        when(serviceLayer.findArchive(inputArchive.getArchiveId())).thenReturn(Optional.ofNullable(inputArchive));
+        when(serviceLayer.updateArchive(newArchive)).thenThrow(IllegalArgumentException.class);
+
+        mockMvc.perform(put("/archive/3")
+                        .content(inputString)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+    /////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
 
     @Test
     public void deleteArchiveTest() throws Exception {
@@ -147,6 +210,20 @@ public class ArchiveControllerTests{
                 )
                 .andDo(print())
                 .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldReturn422DeleteArchive() throws Exception {
+        String inputString = mapper.writeValueAsString(inputArchive);
+
+        doThrow(IllegalArgumentException.class).when(serviceLayer).deleteArchive(inputArchive.getArchiveId());
+
+        mockMvc.perform(delete("/archive/1")
+                        .content(inputString)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
     }
 
 
