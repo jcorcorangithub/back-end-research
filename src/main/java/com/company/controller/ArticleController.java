e0e9d74247879fde685c515a79d720257b7c3986
Your Name <nickmihaly@gmail.com>
2022-03-10 12:41:21 -0800

    added updated article controller test

--- src/main/java/com/company/controller/ArticleController.java
+++ src/main/java/com/company/controller/ArticleController.java
@@ -1,3 +1,4 @@
+
 package com.company.controller;
 
 import com.company.model.Article;
@@ -13,33 +14,23 @@ import java.util.Optional;
 @RestController
 public class ArticleController {
 
-        @Autowired
-        private ServiceLayer serviceLayer;
+    @Autowired
+    private ServiceLayer serviceLayer;
 //        private ArticleRepository articleRepository;
 
-        @GetMapping("/article") // find all articles
-        public List<Article> getAllArticles() {
-                return serviceLayer.findAllArticles();
-        }
+    @GetMapping("/article") // find all articles
+    public List<Article> getAllArticles() {
+        return serviceLayer.findAllArticles();
+    }
 
-        @GetMapping("/article/{articleId}") // find article by articleId
-        public Article getArticleById(@PathVariable int articleId) {
-<<<<<<< HEAD
+    @GetMapping("/article/{articleId}") // find article by String articleId
+    public Article getArticleById(@PathVariable int articleId) {
+        return serviceLayer.findArticle(articleId);
+    }
 
-                Optional<Article> article = articleRepository.findById(articleId);
-
-                if (!article.isPresent()) {
-                        return null;
-                }
-                return article.get();
-=======
-                return serviceLayer.findArticle(articleId);
->>>>>>> 9f60875737fbc2a953afc63594677e42faf9e66b
-        }
-
-        @DeleteMapping("article/{articleId}")
-        @ResponseStatus(value = HttpStatus.NO_CONTENT)
-        public void deleteArticleById(@PathVariable int articleId) {
-                serviceLayer.deleteArticle(articleId);
-        }
+    @DeleteMapping("article/{articleId}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteArticleById(@PathVariable int articleId) {
+        serviceLayer.deleteArticle(articleId);
+    }
 }
